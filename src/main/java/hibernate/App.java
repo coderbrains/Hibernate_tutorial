771b7562598fdbba22ae06889816e55776536126
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-05-30 14:33:00 +0530

    How to creating tables and insert data into it using hibernate

--- src/main/java/hibernate/App.java
+++ src/main/java/hibernate/App.java
@@ -1,6 +1,8 @@
 package hibernate;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -22,7 +24,22 @@ public class App
          
         System.out.println(factory.isClosed());
         
+        Student stu = new Student();
+        stu.setId(102);
+        stu.setName("MANISH");
+        stu.setCity("LUCKNOW");
+        
+        System.out.println(stu);
+        
+        Session session = factory.openSession();
+        
+        Transaction tx =  session.beginTransaction();
+        
+        session.save(stu);
+        
+        tx.commit();
         
         
+        session.close();
     }
 }
